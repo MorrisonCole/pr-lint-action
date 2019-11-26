6f6cbdb20c17c3f15e30d662f0c4c18bb4074093
Morrison Cole <admin@morrisoncole.co.uk>
2019-11-26 11:01:43 +0900

    feat: test title regex

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,35 @@
+import {GitHub} from '@actions/github/lib/github';
+
 const core = require('@actions/core');
 const github = require('@actions/github');
 
 async function run() {
-  try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput} from inside a container`);
+  const githubContext = github.context;
+  const githubToken = core.getInput('github-token');
+  const githubClient: GitHub = new GitHub(githubToken);
 
-    // Get github context data
-    const context = github.context;
-    console.log(`We can even get context data, like the repo: ${context.repo.repo}`)
+  const titleRegex = new RegExp(core.getInput('title-regex'));
+  const title = githubContext.payload.pull_request.title;
+
+  const onFailedRegexComment = core.getInput('on-failed-regex-comment')
+    .replace('%pattern%', titleRegex.source);
+
+  try {
+    if (!titleRegex.test(title)) {
+      const pr = githubContext.issue;
+
+      githubClient.pulls.createReview({
+        owner: pr.owner,
+        repo: pr.repo,
+        pull_number: pr.number,
+        body: onFailedRegexComment,
+        event: 'COMMENT'
+      });
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
+// noinspection JSIgnoredPromiseFromCall
 run();
