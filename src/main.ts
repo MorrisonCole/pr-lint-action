cf8886f018f4324cda79f68545d654d82cf5367f
Tal Borenstein <tal@anecdotes.ai>
2022-02-24 16:53:30 +0200

    Comment when review is not dismissable

--- src/main.ts
+++ src/main.ts
@@ -85,13 +85,23 @@ async function dismissReview(pullRequest: {
         alreadyRequiredChanges(review.state)
       ) {
         core.debug(`Found review to dismiss`);
-        void octokit.rest.pulls.dismissReview({
-          owner: pullRequest.owner,
-          repo: pullRequest.repo,
-          pull_number: pullRequest.number,
-          review_id: review.id,
-          message: onSucceededRegexDismissReviewComment,
-        });
+        if (review.state == 'COMMENTED') {
+          void octokit.rest.pulls.createReviewComment({
+            owner: pullRequest.owner,
+            repo: pullRequest.repo,
+            pull_number: pullRequest.number,
+            review_id: review.id,
+            body: onSucceededRegexDismissReviewComment
+          });
+        } else {
+          void octokit.rest.pulls.dismissReview({
+            owner: pullRequest.owner,
+            repo: pullRequest.repo,
+            pull_number: pullRequest.number,
+            review_id: review.id,
+            message: onSucceededRegexDismissReviewComment,
+          });
+        }
       }
     }
   );
@@ -105,7 +115,7 @@ function isGitHubActionUser(login: string) {
 
 function alreadyRequiredChanges(state: string) {
   // If on-failed-regex-request-changes is set to be true state will be CHANGES_REQUESTED
-  // otherwise the bot will just comment and the state will be COMMENTED
+  // otherwise the bot will just comment and the state will be COMMENTED.
   const stateIsChangesRequested = state == "CHANGES_REQUESTED" || state === "COMMENTED";
   core.debug(`alreadyRequiredChanges output: ${stateIsChangesRequested} (state is: ${state})`);
   return stateIsChangesRequested;
