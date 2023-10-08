45f1c128e376fe19dadd33d0cb99acb4da51fd6d
Morrison Cole <admin@morrisoncole.co.uk>
2023-10-08 10:23:34 +0900

    chore: configure ts-jest with tsconfig

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-};
+  // [...]
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.test.json',
+      },
+    ],
+  },
+}
